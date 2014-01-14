e5fd1a3970bb2729776af4c2f7b0abb35de39b94
hoosin <hoosin@yeah.net>
2014-01-14 15:18:10 +0800

    update

---
+++ Config/Config_Common.php
@@ -0,0 +1,13 @@
+<?php
+class Config_Common {
+
+    const APP_NAME = 'WORK_TIME';
+
+    public static $db = array(
+        'host' => '10.4.3.92',
+        'username' => 'unAho3NX8o1Tx',
+        'password' => 'pzUlKwJdhKnMI',
+        'pnet' => 3306,
+        'dbname' => 'd405932c4b1f748c2ac963289afd3928b',
+    );
+}
