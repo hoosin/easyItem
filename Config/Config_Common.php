a035341ff8582b004e8b7798e63635238d16740c
hoosin <hoosin@yeah.net>
2014-02-27 17:50:05 +0800

    本机配置表

--- Config/Config_Common.php
+++ Config/Config_Common.php
@@ -1,30 +1,12 @@
 <?php
 class Config_Common {
-
     const APP_NAME = 'WORK_TIME';
-
-    // public static $db = array(
-    //     'host' => '10.4.3.92',
-    //     'username' => 'unAho3NX8o1Tx',
-    //     'password' => 'pzUlKwJdhKnMI',
-    //     'pnet' => 3306,
-    //     'dbname' => 'd405932c4b1f748c2ac963289afd3928b',
-    // );
-
     public static $db = array(
-<<<<<<< HEAD
-        'host' => '127.0.0.1',
+         'host' => '127.0.0.1',
         'username' => 'hoosin',
         'password' => 'huxin1989',
         'pnet' => 80,
         'dbname' => 'work_time',
-=======
-        'host' => '10.4.3.92', //主机
-        'username' => 'unAho3NX8o1Tx', //用户名
-        'password' => 'pzUlKwJdhKnMI', //密码
-        'pnet' => 3306, //端口
-        'dbname' => 'd405932c4b1f748c2ac963289afd3928b', //数据库
->>>>>>> 6fc9cd9a9e8efd066c390052c133b27b724126f9
     );
 }
 
